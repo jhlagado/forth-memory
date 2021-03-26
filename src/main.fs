8be4032e452ce3d33e7dc9c42412697ef32d07fe
John Hardy <john.hardy@workjam.com>
2021-03-26 23:32:40 +1100

    first

---
+++ src/main.fs
@@ -0,0 +1,2 @@
+include ttester.fs
+include val.fs
---
+++ src/val.fs
@@ -0,0 +1,10 @@
+t{ 1 VALUE val -> }t
+t{ val -> 1 }t
+
+t{ 3 TO val -> }t
+t{ val -> 3 }t
+
+: setval val SWAP TO val ;
+t{ 5 setval val -> 3 5 }t
+
+." hello "
