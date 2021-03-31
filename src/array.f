52b1e98c211a39fbbff567e307f36c6fc5625324
John Hardy <john.hardy@workjam.com>
2021-04-01 00:45:26 +1100

    literal arrays

--- src/array.f
+++ src/array.f
@@ -1,19 +1,57 @@
+\ "args len" is a group of items on the stack n1 n2 ... followed by len
 
-: array> ( adr size -- n1 n2 ...)       \ array to stack items                  
+depth value mark-depth
+
+: range ( adr len -- adr1 adr2 )        \ convert adr len to address range
+    cells over +
+;
+
+: >array ( args len adr size -- )       \ copies stack items to array                  
+  range
+  cell - 
+  do i ! cell negate +loop              \ count down
+;  
+
+( adr size -- n1 n2... )
+: array>                                \ copies array to stack                  
   range
   swap do i @ cell +loop 
 ;    
 
-: array                                 ( args -- )
-    create
-    dup ,                               \ write len
-    0 do , loop                         \ write each arg
-    does>                               \ addr
-    cell+ dup @                         \ addr1 len
-    swap                                \ len addr1   
-    cell+ swap                          \ addr2 len
-    range                               \ addr1 addr2 
-    cell - do                           \ loop from addr2-1 to addr1
-        i @                             \ read 
-    cell negate +loop                   \ endloop step = -cell
+( idx adr -- val ) 
+: nth                                   \ returns nth item of array
+  2dup @                                \ idx adr idx len
+  U< 0= ABORT" Range error "  
+  cell+ swap cells                      \ adr1 offset
+  + @                                   \ value
 ;
+
+( args len -- adr)
+: new-array                             \ allocates new array from args                   
+  here dup >r                           \ args len adr
+  cell+ swap                            \ args adr1 len
+  dup ,                                 \ write len
+  dup cells allot                       \ args adr1 len 
+  >array                                \ write args
+  r>                                    \ adr
+;
+
+( -- )
+: _[                                    \ starts literal array definition 
+  mark-depth                            \ push current mark-depth
+  depth to mark-depth
+;
+
+( -- adr )
+: ]_                                    \ completes literal array definition 
+  depth mark-depth -                    \ mark args len
+  new-array                             \ mark adr
+  swap                                  \ adr mark
+  to mark-depth                         \ restore mark-depth
+                                        \ adr
+;
+
+_[ 1 2 3 ]_ constant arr
+
+t{ 0 arr nth -> 1 }t
+t{ 2 arr nth -> 3 }t
--- src/main.f
+++ src/main.f
@@ -1,5 +1,4 @@
 include ttester.f
-include mark.f
 include array.f
 include partial.f
 
